97cd80debc32e1bd9d4cff1e2a1149fcce5517f7
Rahul-Raman-R <74867210+Rahul-Raman-R@users.noreply.github.com>
2021-06-10 19:24:51 +0530

    Update app.py

--- app.py
+++ app.py
@@ -4,9 +4,10 @@ import cv2
 import tensorflow as tf
 import numpy as np
 from glob import glob
-#from sklearn.metrics import classification_report
-#from sklearn.model_selection import train_test_split
+from sklearn.metrics import classification_report
+from sklearn.model_selection import train_test_split
 import glob
+import torch
 import os
 import time
 import math
@@ -16,17 +17,44 @@ import face_recognition
 import imutils
 import pandas as pd
 import base64
+import gc
 import plotly.graph_objects as go
 
-
-#from retinaface.pre_trained_models import get_model as get_detector
-model = tf.lite.TFLiteConverter.from_keras_model('my_model1.h5')
+gc.enable()
+from retinaface.pre_trained_models import get_model as get_detector
+#model = tf.keras.models.load_model('my_model1.h5')
 #detector = get_detector("resnet50_2020-07-20", max_size=800)
 
-data = pickle.loads(open("encodings.pickle", "rb").read())
-detector = pickle.loads(open("detector.pickle", "rb").read())
+#data = pickle.loads(open("encodings.pickle", "rb").read())
+#detector = pickle.loads(open("detector.pickle", "rb").read())
+def download_data():
+    
+    path1 = './my_model.h5'
+    path2 = './yolov3.weights'
+    
+    # Local
+    # path1 = './data/LastModelResnet50_v2_16.pth.tar'
+    # path2 = './data/resnet50_captioning.pt'
+    # print("I am here.")
+    
+    if not os.path.exists(path1):
+        decoder_url = 'wget -O ./my_model.h5 https://www.dropbox.com/s/oeu6m85ahsw22ci/yolov3.weights?dl=0'
+        
+        with st.spinner('done!\nmodel was not found, downloading them...'):
+            os.system(decoder_url)
+    else:
+        print("Model 1 is here.")
+
+    if not os.path.exists(path2):
+        encoder_url = 'wget -O ./yolov3.weights https://www.dropbox.com/s/oeu6m85ahsw22ci/yolov3.weights?dl=0'
+        with st.spinner('Downloading yolo weights'):
+            os.system(encoder_url)
+    else:
+        print("Model 2 is here.")
+        
+        
 caught=[]
-def face_recognise(frame):
+def face_recognise(detector, data, frame):
 
     rgb = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
     rgb = imutils.resize(frame, width=750)
@@ -48,11 +76,11 @@ def face_recognise(frame):
 
         if True in matches:
 
-            matchedIND = [i for (i, b) in enumerate(matches) if b]
+            matchedIdxs = [i for (i, b) in enumerate(matches) if b]
             counts = {}
 
 
-            for i in matchedIND:
+            for i in matchedIdxs:
                 name = data["names"][i]
                 counts[name] = counts.get(name, 0) + 1
                 print(counts)
@@ -102,7 +130,7 @@ classes = sorted(['with_mask','without_mask'])
 
 
 
-def visualize_detections_video(image, boxes, mask, nomask,maskper,nomaskper):
+def visualize_detections_video(image,model, boxes, mask, nomask,maskper,nomaskper):
   count_mask=0
   count_nomask=0
   image = np.array(image, dtype=np.uint8)
@@ -118,13 +146,13 @@ def visualize_detections_video(image, boxes, mask, nomask,maskper,nomaskper):
       face_image = tf.image.resize(face_image, [image_w, image_h])
       face_image = face_image/127.5-1
       
-      clas = model.predict(np.expand_dims(face_image,axis=0))
-      clas = np.argmax(clas,axis=1)
-      if clas[0] == 0:
+      _cls = model.predict(np.expand_dims(face_image,axis=0))
+      _cls = np.argmax(_cls,axis=1)
+      if _cls[0] == 0:
         count_mask=count_mask+1
-      elif clas[0] == 1:
+      elif _cls[0] == 1:
         count_nomask= count_nomask+1    
-      text = '{}'.format(class_map[clas[0]])    
+      text = '{}'.format(class_map[_cls[0]])    
       start = tuple(np.array((x,y)).astype('int'))
       end = tuple(np.array((x+w,y+h)).astype('int'))
       mask.text("TOTAL WITH MASK:"+"                                        "+(str(count_mask)))
@@ -133,15 +161,15 @@ def visualize_detections_video(image, boxes, mask, nomask,maskper,nomaskper):
       nomaskperc=(count_nomask/(count_mask+count_nomask))*100  
       maskper.text("MASKED % :"+"                                              "+(str(maskperc)))
       nomaskper.text("WITHOUT MASK % :"+"                                        "+(str(nomaskperc)))  
-      cv2.rectangle(image,start,end,color_map_video[clas[0]],2)
-      cv2.putText(image, class_map[clas[0]], start , cv2.FONT_HERSHEY_SIMPLEX, 0.70, color_map_video[clas[0]], 2)
+      cv2.rectangle(image,start,end,color_map_video[_cls[0]],2)
+      cv2.putText(image, class_map[_cls[0]], start , cv2.FONT_HERSHEY_SIMPLEX, 0.70, color_map_video[_cls[0]], 2)
   return image, maskperc, nomaskperc
 
-def social_dist(img,textp1,textp2,textp3,high, safe):
+def social_dist(net, img,textp1,textp2,textp3,high, safe):
     low_risk_p = 0       
     high_risk_p = 0      
     safe_p = 0
-    net = cv2.dnn.readNet("yolov3.weights", "yolov3.cfg")
+    #net = cv2.dnn.readNet("yolov3.weights", "yolov3.cfg")
     classes = ["person"]
     with open("coco.names", "r") as f:
         classes = [line.strip() for line in f.readlines()]
@@ -294,7 +322,11 @@ def main():
     """COVID-19 Violator App"""
     original_title = '<p style=" font-type:bold; color:#faca2b; font-size: 36px;">COVID-19 VIOLATION MONITOR</p>'
     st.markdown(original_title, unsafe_allow_html=True)
-    
+    download_data()
+    model_path='./my_model.h5'
+    model = torch.load(model_path,map_location='cpu')
+    weights_path='./yolov3.weights'
+    yolov3_weights=torch.load(weights_path,map_location='cpu')
     main_bg = "static/back1.jpg"
     main_bg_ext = "jpg"
     st.markdown(
@@ -313,7 +345,10 @@ def main():
 
     activities = ["Upload","About"]
     choice = st.sidebar.selectbox("MENU",activities)
-
+    model = tf.keras.models.load_model('my_model1.h5')
+    detector = get_detector("resnet50_2020-07-20", max_size=800)
+    data = pickle.loads(open("encodings.pickle", "rb").read())
+    net = cv2.dnn.readNet(yolov3_weights, "yolov3.cfg")
     if choice == 'Upload':
         sub_title = '<p style=" font-type:bold; font-size: 20px;">Here you can upload a video file(.mp4) that you wish to check for violations.</p>'
         st.markdown(sub_title, unsafe_allow_html=True)
@@ -360,12 +395,12 @@ def main():
                 if time_elapsed > 1./frame_rate:
                     result = detector.predict_jsons(image)
                     #print(j)
-                    img1, finhigh, finsafe = social_dist(image,textp1,textp2,textp3,high, safe)
+                    img1, finhigh, finsafe = social_dist(net, image,textp1,textp2,textp3,high, safe)
                     img_array1.append(img1)
                     higharr.append(finhigh)
                     safearr.append(finsafe)
-                    #img2=face_recognise(image)
-                    #img_arr2.append(img2)
+                    img2=face_recognise(detector, data, image)
+                    img_arr2.append(img2)
                     #image_placeholder1.image(img1,channels="BGR") 
                     boxes = []
                     for i in range(len(result)):
@@ -378,7 +413,7 @@ def main():
                     if boxes.size == 0:
                         img_array.append(image)      
                     else:
-                        img, finmask, finnomask=visualize_detections_video(image, boxes, mask, nomask,maskper,nomaskper)
+                        img, finmask, finnomask=visualize_detections_video(image, model, boxes, mask, nomask,maskper,nomaskper)
                         print(finmask)
                         maskarr.append(finmask)
                         nomaskarr.append(finnomask)
@@ -386,7 +421,7 @@ def main():
                         img_array.append(img)
                     start = time.time()
                     #print(start)
-            #print(namearr)
+            print(namearr)
             mask_avg=sum(maskarr)
             mask_avg= (mask_avg/len(maskarr))
             #st.text("AVERAGE MASK PERCENTAGE:"+(str(int(mask_avg))))
@@ -399,10 +434,10 @@ def main():
             high_avg=sum(higharr)
             high_avg= (high_avg/len(higharr))
             #st.text("AVERAGE SOCIAL DISTANCE VIOLATION PERCENTAGE:"+(str(100-int(safe_avg))))
-            #out = cv2.VideoWriter('demo_.avi', 0, 24, (h,w))
+            out = cv2.VideoWriter('demo_.avi', 0, 24, (h,w))
             namelist=list(dict.fromkeys(caught))
-            #s=" ; "
-            #s=s.join(namelist)
+            s=" ; "
+            s=s.join(namelist)
             #st.text("FACES RECOGNIZED WITHOUT MASK:" + s)
             data1=pd.DataFrame(namelist, columns=["RECOGNIZED PEOPLE"])
             my_dict={'ANALYSIS':["AVERAGE MASK PERCENTAGE:","AVERAGE WITHOUT-MASK PERCENTAGE:","AVERAGE SOCIAL DISTANCE MAINTAINED PERCENTAGE:","AVERAGE SOCIAL DISTANCE VIOLATION PERCENTAGE:"],'SUMMARY %':[(int(mask_avg)),(int(nomask_avg)),(int(safe_avg)),(100-int(safe_avg))]}
@@ -419,7 +454,7 @@ def main():
 )
 
             fig.update_yaxes(automargin=True)
-            ##fig.update_layout(margin=dict(l=0,r=0,b=0,t=0),paper_bgcolor='seagreen',font=dict(family="Courier New, monospace",size=16,))      
+            #fig.update_layout(margin=dict(l=0,r=0,b=0,t=0),paper_bgcolor='seagreen',font=dict(family="Courier New, monospace",size=16,))      
             fig1 = go.Figure(data=[go.Table(header=dict(values=list(data1.columns),fill_color='darkslategray',align='left'),cells=dict(values=[data1['RECOGNIZED PEOPLE']],fill_color='teal',align='left',height=30))])
             fig1.update_layout(
     autosize=False,
@@ -444,15 +479,18 @@ def main():
                     out.write(img_arr2[i])
             out.release()
             cap.release()
+            del model, detector, data, net, img_array, img_array1, img_arr2, data1, df, maskarr, nomaskarr, higharr, safearr, namearr, cap 
+            gc.collect()
                                
     elif choice == 'About':
         st.subheader("About")
         st.markdown("Built with Streamlit by Rahul Raman R, Sushmitha J, Sanjana V")
         st.text("The Covid-19 Violator Monitor helps monitor people within a designated\narea,it helps monitor people who are wearing or not wearing a mask and\n also monitorthe social distancing followed by people and the system is\nalso capable ofrecognizing the individuals who are not wearing a mask.\nThis allows the authority/administrators to take appropriate action on\nthose who do not follow the safety guidelines.\n\nThis system can be implemented in educational institutions and workspaces\nto prevent and reduce the spread of covid-19 among students and employees.\nThis would significantly benefit these organizations to start it functions\nin the new norm and this would also provide a sense of safety for\nthose who are part of these organizations.")
         st.success("")
-
-
-
+    #del model, detector, data, net, img_array, img_array1, img_arr2, data1, df, maskarr, nomaskarr, higharr, safearr, namearr, cap 
+    #gc.collect()
+    
+    
 if __name__ == '__main__':
         main()
         
